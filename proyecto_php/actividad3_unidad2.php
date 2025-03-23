c24347b24ae40ac16e2178a6b5a744fc7a7a6699
calderondiego10 <diegorobertocalderon@gmail.com>
2025-03-23 15:49:04 -0600

    first commit

---
+++ proyecto_php/actividad3_unidad2.php
@@ -0,0 +1,7 @@
+<?php
+    $variable_nombre = "Diego Roberto Calderon";
+    echo "Hola mundo, mi nombre es" . $variable_nombre;
+
+?>
+<br>
+<img src="Flux_Dev_A_detailed_illustration_of_Gandalf_the_Grey_from_JRR__3.jpeg"/>
